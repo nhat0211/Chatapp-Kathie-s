06c76a043f5a4936a7972f9c98cb30714b043f86
nhat0211 <50439597+nhat0211@users.noreply.github.com>
2024-05-03 15:20:35 +0700

    ionic-test

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.Chatapp.Kathie',
+  appName: 'Chatapp_Kahtie',
+  webDir: 'www',
+};
+
+export default config;
